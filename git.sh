9aec375dc6212b27ecca75462e1713abd54147a6
hary868 <mhk867@gmail.com>
2024-02-13 00:47:09 +0530

    second push of everything

--- git.sh
+++ git.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 git remote remove origin
-commit='first push of everything'
+commit='second push of everything'
 reponame='Docker'
 url="https://github.com/hary8678/Notes-$reponame.git"
 git init
